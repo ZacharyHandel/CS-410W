d6f3cc3b27123cb6c75b1494f982be22c02698e9
Zachary Handel <zachary.handelzh@gmail.com>
2023-02-01 21:38:45 -0600

    Assigment 2

--- Fortran_Code/A2_1.f90
+++ Fortran_Code/A2_1.f90
@@ -1,4 +1,34 @@
 PROGRAM A2_1
+    !Read employee's number of hours worked
+    !Read employee's basic rate
+
+    !IF the hours worked exceed 60, print error
+        !Else, print expected statement
+
+    REAL :: numberOfHours
+    REAL :: overtimeHours
+    REAL :: basicRate
+    REAL :: pay
+    WRITE(*,*) "Number of hours worked: "
+    READ(*,*) numberOfHours
+    WRITE(*,*) "Basic Rate: "
+    READ(*,*) basicRate
+
+    IF(basicRate > 15) THEN
+        WRITE(*,*) "ERROR: Invalid pay rate. Enter a pay rate lower than $15.00"
+    ELSE IF(numberOfHours > 60) THEN
+        WRITE(*,*) "ERROR: Too many hours inputted. Contact management."
+    ELSE IF(numberOfHours >=40 .AND. numberOfHours <= 60) THEN
+        overtimeHours = numberOfHours - 40
+        WRITE(*,*) "OVERTIME HOURS DEBUG: ", overtimeHours
+        WRITE(*,*) "REMAINING HOURS DEBUG: ", remainingHours
+        pay = (overtimeHours * (basicRate * 1.5)) + (basicRate * 40)
+        WRITE(*,*) "Pay: ", pay
+    ELSE IF(pay <= 40) THEN
+        pay = numberOfHours * basicRate
+        WRITE(*,*) "Pay: ", pay
+    
 
 
+    END IF
 END PROGRAM A2_1
